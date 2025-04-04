9f58572d5fa8e68a489fceb614a5b09c14210e51
MeepoohCH <89334062+MeepoohCH@users.noreply.github.com>
2025-04-04 13:42:53 +0700

    delete api outside

--- app/node_stack/data.js
+++
@@ -1,8 +0,0 @@
-export async function getDataStack() {
-    const res = await fetch('http://localhost:3001/node_stack/api')
-    if (!res.ok){
-        throw new Error('Failed to fetch data')
-    }
-    return res.json()
-}
-
